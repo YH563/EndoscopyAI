1ae9220586cd29d1316725cb4c4f91bf0e08a02f
延楷 刘 <3276760037@qq.com>
2025-04-14 15:56:13 +0800

    实现了直方图均衡化、各向异性扩散滤波和白平衡矫正

--- EndoscopyAI/EndoscopyAI/Views/MainWindow.xaml.cs
+++ EndoscopyAI/EndoscopyAI/Views/MainWindow.xaml.cs
@@ -88,5 +88,71 @@ namespace EndoscopyAI.Views
                 }
             }
         }
+
+        private void ImgEnhance(object sender, RoutedEventArgs e)
+        {
+            if (_currentImage == null)
+            {
+                MessageBox.Show("尚未导入图像！", "错误", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            try
+            {
+                // 图像增强处理
+                var imageProcess = new ImageProcess();
+                _currentImage = imageProcess.HistogramEqualization(_currentImage); // 更新 _currentImage
+
+                // 显示增强后的图像
+                _imageDisplay.DisplayImage(ImageDisplay, _currentImage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"图像增强时出错: {ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ImgDenoise(object sender, RoutedEventArgs e)
+        {
+            if (_currentImage == null)
+            {
+                MessageBox.Show("尚未导入图像！", "错误", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            try
+            {
+                // 图像去噪处理
+                var imageProcess = new ImageProcess();
+                _currentImage = imageProcess.AnisotropicDiffusion(_currentImage, iterations: 15, kappa: 30.0f); // 更新 _currentImage
+
+                // 显示去噪后的图像
+                _imageDisplay.DisplayImage(ImageDisplay, _currentImage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"图像去噪时出错: {ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void WhiteBalance(object sender, RoutedEventArgs e)
+        {
+            if (_currentImage == null)
+            {
+                MessageBox.Show("尚未导入图像！", "错误", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            try
+            {
+                // 图像白平衡处理
+                var imageProcess = new ImageProcess();
+                _currentImage = imageProcess.WhiteBalance(_currentImage); // 更新 _currentImage
+
+                // 显示白平衡调整后的图像
+                _imageDisplay.DisplayImage(ImageDisplay, _currentImage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"白平衡调整时出错: {ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
